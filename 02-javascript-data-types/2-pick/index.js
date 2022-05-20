e05b2340f63ab3ad8e45f303203291f3126acb81
Andrey Romanychev <andrey.romanychev@insales.ru>
2022-05-20 06:47:39 +0300

    [2.2] pick

--- 02-javascript-data-types/2-pick/index.js
+++ 02-javascript-data-types/2-pick/index.js
@@ -5,5 +5,6 @@
  * @returns {object} - returns the new object
  */
 export const pick = (obj, ...fields) => {
-
+  const subset = Object.fromEntries(fields.filter(key => key in obj).map(key => [key, obj[key]]));
+  return subset;
 };
