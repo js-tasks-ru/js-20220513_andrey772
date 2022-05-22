e8940fde8a80a17dbb8b6a8990d85a28fe669cec
Andrey Romanychev <andrey.romanychev@insales.ru>
2022-05-22 07:52:15 +0300

    [3.3] trim identical symbols

--- 03-objects-arrays-intro-to-testing/3-trim-symbols/index.js
+++ 03-objects-arrays-intro-to-testing/3-trim-symbols/index.js
@@ -5,5 +5,19 @@
  * @returns {string} - the new string without extra symbols according passed size
  */
 export function trimSymbols(string, size) {
-
+  let identicalSymbolsArray = [];
+  let prevChar = '';
+  let j = -1;
+  // xxxaaaaab -> ["xxx", "aaaaa", "b"]
+  for (let i = 0; i < string.length; i++) {
+    let char = string.charAt(i);
+    if (prevChar === char) {
+      identicalSymbolsArray[j] += char;
+    } else {
+      identicalSymbolsArray[++j] = char;
+    }
+    prevChar = char;
+  }
+  // ["xxx", "aaaaa", "b"] -> 'xxaab'
+  return identicalSymbolsArray.map((identicals) => identicals.slice(0, size)).join('');
 }
