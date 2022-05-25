0b80649e895653148683e6c9516ec64cbbdda471
Andrey Romanychev <andrey.romanychev@insales.ru>
2022-05-25 05:50:11 +0300

    [3.1][3.2] corrected the remarks after review

--- 03-objects-arrays-intro-to-testing/1-create-getter/index.js
+++ 03-objects-arrays-intro-to-testing/1-create-getter/index.js
@@ -4,7 +4,9 @@
  * @returns {function} - function-getter which allow get value from object by set path
  */
 export function createGetter(path) {
+  const pathArray = path.split(".");
+
   return function (obj) {
-    return path.split(".").reduce((prev, curr) => prev && prev[curr], obj);
+    return pathArray.reduce((prev, curr) => prev && prev[curr], obj);
   };
 }
--- 03-objects-arrays-intro-to-testing/2-invert-object/index.js
+++ 03-objects-arrays-intro-to-testing/2-invert-object/index.js
@@ -4,8 +4,7 @@
  * @returns {object | undefined} - returns new object or undefined if nothing did't pass
  */
 export function invertObj(obj) {
-  if (typeof obj === 'undefined') {
-    return undefined;
-  }
+  if (typeof obj === 'undefined') return;
+
   return Object.fromEntries(Object.entries(obj).map(([k, v]) => [v, k]));
 }
